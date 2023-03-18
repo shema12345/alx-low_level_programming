0af91f7e5df863aa46bd5720da734a8dbe4b176b
shema12345 <shemaaimeeris@gmail.com>
2023-03-17 20:01:40 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -22,8 +22,11 @@ int main(void)
 					putchar(i + '0');
 					putchar(j + '0');
 					putchar(k + '0');
+					if (i + j + k != 24)
+					{
 					putchar(',');
 					putchar(' ');
+					}
 				}
 			}
 		}
