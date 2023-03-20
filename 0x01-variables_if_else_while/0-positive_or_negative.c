099d46425092d107de32524b95bd69b965adaf11
shema12345 <shemaaimeeris@gmail.com>
2023-03-20 12:01:39 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,28 +2,34 @@
 #include <time.h>
 /* more headers goes here */
 #include <stdio.h>
-/* betty style doc for function main goes here */
+/* betty style doc for function main goes ther */
 /**
  * main - main function
  *
- * Retun: always o
+ * Return: always 0
  */
-
-int main(void)
+int main (void)
 {
-	int n;
-	
-	srand(time(0));
-	n = 0;
-       	rand();
-	RAND_MAX / 2;
-	/* your code goes there */
-	if (n > 0)
+        int n;
+        srand(time(0));
+        n = rand() - RAND_MAX /2;
+        /* your code goes there */
+	if (n >0)
+	{
 		printf("%d is positive", n);
-	else if (n == 0)
-		printf("%d is zero", n);
+	}
 	else
+	{
+		if (n == 0)
+		{
+		printf("%d is zero", n);
+		}
+	else
+	{
 		printf("%d is negative", n);
+	}
+	}
 	printf("\n");
-	return (0);
+        return (0);
 }
+
