b2967ac3150b34040e30acea03d910599853df62
shema12345 <shemaaimeeris@gmail.com>
2023-03-21 08:11:01 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - this is the main function
+ * _putchar: print the alphabetics putchar
+ * Return: always 0
+ */
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
